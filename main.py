f11ed77b88d57250fb830c5636b2048b3f75e3b1
Logan <zaid.goulfarris@electrum.co.za>
2025-02-24 11:39:25 +0200

    draw player

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     # Initialise pygame
@@ -11,6 +12,13 @@ def main():
     
     # Create a game window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Create a clock to limit FPS
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # Create player in the middle of the screen
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     # Game loop
     while True:
@@ -22,8 +30,14 @@ def main():
         # Fill the screen with black
         screen.fill((0, 0, 0))
 
+        # Draw player
+        player.draw(screen)
+
         # Update the display
         pygame.display.flip()
+
+        # Cap the frame rate to 60 FPS and calculate delta time
+        dt = clock.tick(60) / 1000 # Convert ms to seconds
 
 if __name__ == "__main__":
     main()
